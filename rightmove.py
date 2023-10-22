6351483ddbf860ecc84b9fd1ff965e04493d3176
alirezamiim <35329235+alirezamiim@users.noreply.github.com>
2023-10-22 16:25:20 +0100

    Delete rightmove.py

--- rightmove.py
+++
@@ -1,8 +0,0 @@
-import requests
-import csv
-
-region='%5E87494'
-page_index = '&index=48'
-url = 'https://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=REGION'+ \
-        region+'&maxBedrooms=1&maxPrice=1300&radius=3.0'+page_index \
-        +'&propertyTypes=flat&includeLetAgreed=false&mustHave=&dontShow=&furnishTypes=furnished&keywords='
